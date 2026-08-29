ba0db70b75422a9f2560024b7de222d0e8ba8a80
B2go-crypto <ibitugo@gmail.com>
2026-08-30 02:19:13 +0300

    Update pi-sdk.js

--- pi-sdk.js
+++ pi-sdk.js
@@ -1,10 +1,1 @@
-const Pi = {
-    init: function(config) {
-        console.log("Pi SDK initialized");
-    },
-    authenticate: function(scopes, onIncompletePayment) {
-        return new Promise((resolve) => {
-            resolve({ user: { uid: "demo-user" } });
-        });
-    }
-};
+Pi.init({ version: "2.0" });
